14883e29d05b7d0e0c3268b7bf8b2f44386850f5
jasper <cool.daniel1234@gmail.com>
2020-06-22 15:01:46 +0800

    Initial Commit

---
+++ BINDING/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BINDING
+//
+//  Created by User02 on 2020/6/22.
+//  Copyright © 2020 sun. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
